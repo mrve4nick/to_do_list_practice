cc619c676f54fd540ef73fa7ac47ff8ca3703dd9
ve4nick <ve4nick98@gmail.com>
2025-05-09 17:50:05 +0300

    Create views for index and tags list view

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic, View
+from to_do_list.models import Tag
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "to_do_list/index.html"
+
+
+class TagsListView(generic.ListView):
+    model = Tag
+    context_object_name = "task_list"
+    template_name = "to_do_list/tags_list.html"
