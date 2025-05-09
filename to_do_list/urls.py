359298fb7f652b79eaff7af329a04bebb93c7cf9
ve4nick <ve4nick98@gmail.com>
2025-05-09 20:15:25 +0300

    Update names in urls

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("tags/", views.TagsListView.as_view(), name="tags-list-view"),
-    path("tag/create", views.TagCreateView.as_view(), name="task-create-view"),
-    path("tag/<int:pk>/update", views.TagUpdateView.as_view(), name="task-update-view"),
-    path("tag/<int:pk>/delete", views.TagDeleteView.as_view(), name="task-delete-view"),
+    path("tag/create", views.TagCreateView.as_view(), name="tag-create-view"),
+    path("tag/<int:pk>/update", views.TagUpdateView.as_view(), name="tag-update-view"),
+    path("tag/<int:pk>/delete", views.TagDeleteView.as_view(), name="tag-delete-view"),
     path("task/create", views.TaskCreateView.as_view(), name="task-create-view"),
     path("task/<int:pk>/update", views.TaskUpdateView.as_view(), name="task-update-view"),
     path("task/<int:pk>/delete", views.TaskDeleteView.as_view(), name="task-delete-view"),
