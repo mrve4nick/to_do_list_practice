f6c19ed8a06d3948074d8a237529371829449040
ve4nick <ve4nick98@gmail.com>
2025-05-09 17:49:05 +0300

    Add app_name and url for tags list view

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name="index"),
+    path('tags/', views.TagsListView.as_view(), name="tags-list-view"),
 ]
+
+app_name = "to_do_list"
