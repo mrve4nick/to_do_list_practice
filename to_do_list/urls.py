64a870cd8ad76a9a71b9e786d25a7e386a451997
ve4nick <ve4nick98@gmail.com>
2025-05-09 20:13:36 +0300

    Add task urls

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    path('tags/', views.TagsListView.as_view(), name="tags-list-view"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("tags/", views.TagsListView.as_view(), name="tags-list-view"),
+    path("tag/create", views.TagCreateView.as_view(), name="task-create-view"),
+    path("tag/<int:pk>/update", views.TagUpdateView.as_view(), name="task-update-view"),
+    path("tag/<int:pk>/delete", views.TagDeleteView.as_view(), name="task-delete-view"),
+    path("task/create", views.TaskCreateView.as_view(), name="task-create-view"),
+    path("task/<int:pk>/update", views.TaskUpdateView.as_view(), name="task-update-view"),
+    path("task/<int:pk>/delete", views.TaskDeleteView.as_view(), name="task-delete-view"),
+    path("task/<int:pk>/toggle", views.toggle_task_status, name="task-toggle-view"),
 ]
 
 app_name = "to_do_list"
