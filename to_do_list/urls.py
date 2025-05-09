eb47e784a27985fc1f735ff92e2e0e4006629aa8
ve4nick <ve4nick98@gmail.com>
2025-05-09 20:41:32 +0300

    Refactor views and names

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path("task/create", views.TaskCreateView.as_view(), name="task-create-view"),
     path("task/<int:pk>/update", views.TaskUpdateView.as_view(), name="task-update-view"),
     path("task/<int:pk>/delete", views.TaskDeleteView.as_view(), name="task-delete-view"),
-    path("task/<int:pk>/toggle", views.toggle_task_status, name="task-toggle-view"),
 ]
 
 app_name = "to_do_list"
